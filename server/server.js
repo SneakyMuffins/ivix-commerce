9b4f69f0674811d978fbd9d5f195de8084b6c0a6
Misho Papiashvili <mishopapiashvili@gmail.com>
2023-10-25 19:18:26 +0400

    Bootstrapping the server: Created the graphQL endpoint and product schema, Added images dir and served static images from the directory, Created product route and product data

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const path = require('path');
+const { schema, root } = require('./routes/product');
+
+const app = express();
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log('GraphQL server is running at http://localhost:4000/graphql');
+});
